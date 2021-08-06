27fd0820c5d50b3da877e2a606c5f6297736131b
magesh <hariarunengg@gmail.com>
2021-08-06 09:47:42 +0530

    day-2 bug fix-2

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,37 +1,37 @@
 function Footer() {
   return (
-    <div className="grid grid-cols-1 bg-[#050404]  md:grid-cols-4   gap-y-10 py-14 px-32 bg-gray-100 text-gray-600">
+    <div className="grid grid-cols-1  md:grid-cols-4   gap-y-10 py-14 px-32 bg-gray-100 text-gray-600">
       {/* About */}
-      <div className="sm:border-gray-500 space-y-4">
-        <h5 className="font-bold text-gray-300"> About</h5>
-        <p className="cursor-pointer">How Airbnb Works</p>
-        <p className="cursor-pointer">Newsroom</p>
-        <p className="cursor-pointer">Investors</p>
-        <p className="cursor-pointer">Airbnb Plus</p>
+      <div className="space-y-4">
+        <h5 className="font-bold text-gray-800"> About</h5>
+        <p>How Airbnb Works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
       </div>
       {/* Community */}
       <div className="space-y-4">
-        <h5 className="font-bold text-gray-300"> COMMUNITY</h5>
-        <p className="cursor-pointer">Diversity & Belonging</p>
-        <p className="cursor-pointer">Its a pretty awesome Clone</p>
-        <p className="cursor-pointer">Referrals accepted</p>
-        <p className="cursor-pointer">Papa React </p>
+        <h5 className="font-bold"> COMMUNITY</h5>
+        <p>Diversity & Belonging</p>
+        <p>Its a pretty awesome Clone</p>
+        <p>Referrals accepted</p>
+        <p>Papa React </p>
       </div>
       {/* Host */}
       <div className="space-y-4">
-        <h5 className="font-bold text-gray-300"> HOST</h5>
-        <p className="cursor-pointer">Host your home</p>
-        <p className="cursor-pointer">Host an Online Experience</p>
-        <p className="cursor-pointer">Responsible hosting</p>
-        <p className="cursor-pointer">Host an Experience</p>
+        <h5 className="font-bold"> HOST</h5>
+        <p>Host your home</p>
+        <p>Host an Online Experience</p>
+        <p>Responsible hosting</p>
+        <p>Host an Experience</p>
       </div>
       {/* Support */}
       <div className="space-y-4">
-        <h5 className="font-bold text-gray-300"> SUPPORT</h5>
-        <p className="cursor-pointer">Our COVID-19 Response</p>
-        <p className="cursor-pointer">Its a pretty awesome Clone</p>
-        <p className="cursor-pointer">Help Centre</p>
-        <p className="cursor-pointer">Cancellation options </p>
+        <h5 className="font-bold"> SUPPORT</h5>
+        <p>Our COVID-19 Response</p>
+        <p>Its a pretty awesome Clone</p>
+        <p>Help Centre</p>
+        <p>Cancellation options </p>
       </div>
     </div>
   );
